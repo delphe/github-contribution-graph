89f67e207eec409c4857d97a8c2070e6f9dc66ae
Edwin J.E. Delph <delphedwin@gmail.com>
2020-02-16 09:52:13 -0700

    resolve errors in unit tests

--- src/app/githubapi.service.spec.ts
+++ src/app/githubapi.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GitHubApiService } from './githubapi.service';
 
 describe('GithubapiService', () => {
   let service: GitHubApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    
     service = TestBed.inject(GitHubApiService);
   });
 
